86418193e0920678fe58b6f1473e670b8880b2a5
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2024-07-21 17:24:25 +0530

    fix: add links

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
 import "./App.css";
 
-const files = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"];
+const files = [
+  "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/10PRINT.webm",
+  "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/Sine%20Waves.webm",
+  "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/Metaballs.webm",
+  "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/Pulsing%20Circle.webm",
+  "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/2d%20Raycasting.webm",
+  "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/3D%20Perlin%20Terrain%20.mp4",
+  "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/Atom.webm",
+  "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/Breathing%20Cuboids.webm",
+  "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/Cube%20Points.webm",
+  "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/Flow%20Field.webm",
+  "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/Network%20Particles.webm",
+  "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/Sinusoidal%20Sphere.mp4",
+];
 function App() {
   return (
     <div
@@ -10,13 +23,15 @@ function App() {
         display: "flex",
         flexWrap: "wrap",
         margin: "0 auto",
-        gap: "8px",
+        gap: "16px",
         justifyContent: "center",
       }}
     >
       {files.map((file) => (
         <div className="card" key={file}>
-          {file}
+          <video autoPlay loop muted controls={false}>
+            <source src={file} type={"video/" + file.split(".").pop()} />
+          </video>
         </div>
       ))}
     </div>
