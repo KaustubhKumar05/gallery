e53d29a40534b7eb8d49cb6a27987524cccf2efd
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2024-07-21 17:38:32 +0530

    fix: links, css

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 const files = [
   "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/10PRINT.webm",
   "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/Sine%20Waves.webm",
-  "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/Metaballs.webm",
+  "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/Metaballs.mp4",
   "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/Pulsing%20Circle.webm",
   "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/2d%20Raycasting.webm",
   "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/3D%20Perlin%20Terrain%20.mp4",
