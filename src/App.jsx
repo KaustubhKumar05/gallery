7acc50d103a1ed5d7be3f0fb1edec9bac79131df
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2024-07-22 23:16:45 +0530

    feat: drag and drop

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
+import { useState } from "react";
+import { sketches } from "./constants";
 import "./App.css";
 
-const files = [
-  "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/10PRINT.webm",
-  "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/Sine%20Waves.webm",
-  "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/Metaballs.mp4",
-  "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/Pulsing%20Circle.webm",
-  "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/2d%20Raycasting.webm",
-  "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/3D%20Perlin%20Terrain%20.mp4",
-  "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/Atom.webm",
-  "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/Breathing%20Cuboids.webm",
-  "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/Cube%20Points.webm",
-  "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/Flow%20Field.webm",
-  "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/Network%20Particles.webm",
-  "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/Sinusoidal%20Sphere.mp4",
-];
 function App() {
+  const [files, setFiles] = useState(sketches);
+
+  const handleDrop = (e, id) => {
+    e.preventDefault();
+    const draggedId = parseInt(e.dataTransfer.getData("text"));
+
+    if (draggedId !== id) {
+      const newFiles = [...files];
+      const draggedIndex = newFiles.findIndex((file) => file.id === draggedId);
+      const targetIndex = newFiles.findIndex((file) => file.id === id);
+
+      [newFiles[draggedIndex], newFiles[targetIndex]] = [
+        newFiles[targetIndex],
+        newFiles[draggedIndex],
+      ];
+
+      setFiles(newFiles);
+    }
+  };
   return (
     <div
       style={{
@@ -23,15 +30,36 @@ function App() {
         display: "flex",
         flexWrap: "wrap",
         margin: "0 auto",
-        gap: "16px",
+        gap: "1rem",
         justifyContent: "center",
       }}
     >
       {files.map((file) => (
-        <div className="card" key={file}>
-          <video autoPlay loop muted controls={false}>
-            <source src={file} type={"video/" + file.split(".").pop()} />
+        <div
+          className="card"
+          key={file.link}
+          onDragStart={(e) => {
+            e.dataTransfer.setData("text/plain", file.id);
+          }}
+          onDragOver={(e) => e.preventDefault()}
+          onDrop={(e) => handleDrop(e, file.id)}
+          draggable
+        >
+          <video
+            draggable={false}
+            autoPlay
+            loop
+            muted
+            controls={false}
+            onMouseEnter={(e) => e.target.pause()}
+            onMouseLeave={(e) => e.target.play()}
+          >
+            <source
+              src={file.link}
+              type={"video/" + file.link.split(".").pop()}
+            />
           </video>
+          <p>{file.name}</p>
         </div>
       ))}
     </div>
---
+++ src/constants.js
@@ -0,0 +1,62 @@
+export const sketches = [
+    {
+      id: 0,
+      link: "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/10PRINT.webm",
+      name: "10PRINT",
+    },
+    {
+      id: 1,
+      link: "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/Sine%20Waves.webm",
+      name: "Sine Waves",
+    },
+    {
+      id: 2,
+      link: "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/Metaballs.mp4",
+      name: "Metaballs",
+    },
+    {
+      id: 3,
+      link: "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/Pulsing%20Circle.webm",
+      name: "Pulsing Circle",
+    },
+    {
+      id: 4,
+      link: "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/2d%20Raycasting.webm",
+      name: "Raycasting",
+    },
+    {
+      id: 5,
+      link: "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/3D%20Perlin%20Terrain%20.mp4",
+      name: "Perlin Terrain",
+    },
+    {
+      id: 6,
+      link: "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/Atom.webm",
+      name: "Atom",
+    },
+    {
+      id: 7,
+      link: "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/Breathing%20Cuboids.webm",
+      name: "Breathing Cuboids",
+    },
+    {
+      id: 8,
+      link: "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/Cube%20Points.webm",
+      name: "Cube Points",
+    },
+    {
+      id: 9,
+      link: "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/Flow%20Field.webm",
+      name: "Flow Field",
+    },
+    {
+      id: 10,
+      link: "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/Network%20Particles.webm",
+      name: "Network Particles",
+    },
+    {
+      id: 11,
+      link: "https://pub-11db8ea9885840f8aeaf8260c9cedd44.r2.dev/Sinusoidal%20Sphere.mp4",
+      name: "Sinusoidal Sphere",
+    },
+  ]
